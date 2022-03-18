fc2b3e7103e91570de25bf89edd018e739d94b78
anton shabanov <89005421555@mail.ru>
2022-03-18 15:28:36 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("ВАШ МОБИЛЬНЫЙ СЧЕТ.");
+
+        int account = 500;
+        int payment = 1000;
+        int bonus = payment / 100;
+
+
+
+
+        if (payment >= 200) {
+            System.out.println(bonus);
+        }
+
+        int all = account + payment + bonus;
+
+        System.out.println("На Ваш счет зачислено: " + all);
+
+
+
+
+    }
+}
